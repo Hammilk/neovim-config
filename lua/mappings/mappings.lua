81a03f050f1068725b39a0bce56f16b34b13302a
Hammilk <khai1995pham@gmail.com>
2024-10-16 01:47:50 -0500

    switched from packer to lazy

---
+++ lua/mappings/mappings.lua
@@ -0,0 +1,11 @@
+-- Keybindings (example)
+vim.g.mapleader = ' '
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>t', ':terminal<CR>', { noremap = true, silent = true })
+-- Map keys for buffer navigation
+vim.api.nvim_set_keymap('n', '<leader>bn', ':BufferLineCycleNext<CR>', { noremap = true, silent = true }) -- Next buffer
+vim.api.nvim_set_keymap('n', '<leader>bp', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true }) -- Previous buffer
+
+
+
---
+++ lua/settings/settings.lua
@@ -0,0 +1,11 @@
+-- General settings
+vim.opt.number = true  -- Show line numbers
+vim.opt.relativenumber = true  -- Show relative line numbers
+vim.opt.tabstop = 4  -- Number of spaces for a tab
+vim.opt.shiftwidth = 4  -- Number of spaces to use for each indentation
+vim.opt.expandtab = true  -- Convert tabs to spaces
+vim.opt.smartindent = true  -- Automatically indent new lines
+vim.opt.ignorecase = true -- Ignore case when searching
+vim.opt.smartcase = true -- Override ignore case if search ontains uppercase letters
+
+
